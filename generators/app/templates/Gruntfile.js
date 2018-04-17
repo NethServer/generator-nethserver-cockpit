c3625c7735f8855467b976c1e7f938de84997d69
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2018-04-17 15:58:30 +0200

    gruntjs. added new uglify in build task

--- generators/app/templates/Gruntfile.js
+++ generators/app/templates/Gruntfile.js
@@ -105,7 +105,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-usemin');
